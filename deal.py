e06352008d3ceca2391c2edf036a63224aa384c6
prtitrz <prtitrz@gmail.com>
2012-06-13 17:04:20 +0800

    add deal trace code(ugly way)

---
+++ deal.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import os
+
+def main():
+    with open("/home/rz/Trace/WebSearch2.spc") as f, open("tmpfile", "r+") as g:
+        for line in f:
+            num, offsize, length, op, sec = line.split(',');
+            n = int(num)
+            if n > 2: continue
+            if n == 2: n = 6
+            if n == 1: n = 3
+            o = int(offsize)
+            if o > 22000000:
+                n += 2
+                o -= 22000000
+            elif o > 11000000 and o < 22000000:
+                n += 1
+                o -= 11000000
+            num = str(n)
+            offsize = str(o)
+            deal = (",").join([num, offsize, length, op, sec])
+            g.write(deal)
+
+if __name__ == '__main__':
+    main()
